40564e77e525936cdc73ae3b4903ef3ff2a8f823
toonew <litaixin01@163.com>
2017-08-02 15:39:39 +0800

    修改提交require  db文件为 到处db文件位置

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /**
  * Created by toonew on 2017/8/2.
  */
+const path = require('path');
 const ip2region = require('./lib/ip2region');
-const ip2regionDB = require('./lib/ip2region.db');
 const mapRegion = require('./lib/map_region');
 const ipAreaUtil = require('./lib/ip_area_util');
 
+const ip2regionDBPath = path.join(__dirname, './lib/ip2region.db');
+
 module.exports = {
   ip2region,
-  ip2regionDB,
+  ip2regionDBPath,
   mapRegion,
   ipAreaUtil
 };
+
+console.log(ip2regionDBPath);
